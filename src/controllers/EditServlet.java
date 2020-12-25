3537ba73a400910d1b2f2e7312d3f1a3790aa2a7
yuki.Miya <yuki.miya0330@gmail.com>
2020-12-25 22:07:49 +0900

    Add EditServlet

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -33,13 +33,16 @@ public class EditServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        Task t = em.find(Task.class, request.getSession().getId());
+        Task t = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
 
         em.close();
 
+        request.setAttribute("task", t);
+        request.setAttribute("_token", request.getSession().getId());
+
         request.getSession().setAttribute("task_id", t.getId());
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit/jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
     }
 
