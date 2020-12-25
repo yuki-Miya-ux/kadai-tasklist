70260823820ab3e6cf6f5ec61839c0ac6145845e
yuki.Miya <yuki.miya0330@gmail.com>
2020-12-25 12:14:38 +0900

    Add Modify Index/NewServlet

---
+++ WebContent/WEB-INF/views/tasks/index.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:import url="../layout/app.jsp">
+    <c:param name="content">
+        <h2>TODO LIST</h2>
+        <ul>
+            <c:forEach var="task" items="${task}">
+                <li>
+                    <a href="${pageContext.request.contextPath}/show?id=${task.id}">
+                        <c:out value="${task.id}"/>
+                    </a>
+                </li>
+            </c:forEach>
+        </ul>
+
+        <p><a href="${pageContext.request.contextPath}/new">新規の登録</a></p>
+
+    </c:param>
+</c:import>
--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,7 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
 
-import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,19 +10,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import utils.DBUtil;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class NewServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public NewServlet() {
         super();
     }
 
@@ -32,15 +29,11 @@ public class IndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
+        request.setAttribute("_token", request.getSession().getId());
 
-        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
+        request.setAttribute("task", new Task());
 
-        em.close();
-
-        request.setAttribute("tasks", tasks);
-
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
         rd.forward(request, response);
     }
 
